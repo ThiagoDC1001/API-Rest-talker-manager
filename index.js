bfdaaff3688a6626cde528a59025e2e246a46821
ThiagoDC1001 <cmsthiagoazevedo@gmail.com>
2022-08-07 18:06:02 -0300

    Verificação de email e senha funcionando

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const validacaoDados = (email, password) => {
+  const validEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  if (!email) return { status: 400, message: 'O campo "email" é obrigatório' };
+  if (!email.match(validEmail)) {
+    return { status: 400, message: 'O "email" deve ter o formato "email@email.com"' };
+  }
+  if (!password) return { status: 400, message: 'O campo "password" é obrigatório' };
+  if (password.length < 6) {
+  return { status: 400, message: 'O "password" deve ter pelo menos 6 caracteres' }; 
+  }
+  return '';
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -32,9 +45,13 @@ app.get('/talker/:id', async (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  const token = tokenGenerator.generate(16);  
-  // const tokenPadrao = { token: '`${token}`' };
-  res.status(200).send({ token });
+  const { email, password } = req.body;
+  const token = tokenGenerator.generate(16);    
+  const validate = validacaoDados(email, password);
+  if (validate.status) {
+    return res.status(validate.status).send({ message: validate.message });
+  }
+  return res.status(200).send({ token });
 });
 
 app.listen(PORT, () => {
